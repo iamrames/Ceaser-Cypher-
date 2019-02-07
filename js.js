2e0e8a8f9e55b3a30edabd16c5619642ad2f6274
iamrames <addictedtomig@gmail.com>
2019-02-07 12:01:10 +0545

    Ceaser Cypher Javascript

---
+++ js.js
@@ -0,0 +1,44 @@
+var cypherText = [];
+var decryptedText = [];
+
+function Encrypt(key,string){
+	return string.split("")
+		.map(char => {
+			let code = char.charCodeAt();
+			if(code >= 65 && code <= 90)
+				return String.fromCharCode(((code + key - 65)%26) + 65);
+			else if(code >= 97 && code <= 122)
+				return String.fromCharCode(((code + key - 97)%26)+97);
+			else if(code == 32)
+				return '$';
+		}).join('');
+}
+
+function Decrypt(key,string){
+	return string.split("")
+		.map(char => {
+			let code = char.charCodeAt();
+			if(code >= 65 && code <= 90)
+				return String.fromCharCode(((code - key + 26 -65)%26) + 65);
+			else if(code >= 97 && code <= 122)
+				return String.fromCharCode(((code - key + 26 - 97)%26) + 97);
+			else if(code == 36)
+				return ' ';
+		}).join('');
+}
+
+// Your file location
+let file = 'D:\OS.txt';
+
+var fs = require('fs');
+var textByLine = fs.readFileSync(file).toString().split("\n");
+
+for(let line of textByLine)
+	cypherText.push(Encrypt(3,line));
+
+for(let line of cypherText)
+	decryptedText.push(Decrypt(3,line));
+
+console.log(cypherText);
+console.log(decryptedText);
+
